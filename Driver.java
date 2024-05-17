20ba6fd0ba86b8d539104bdc8ca7dba8cafb18a4
Aidan Wong <awong50@stuy.edu>
2024-05-17 08:40:57 -0400

    Implemented add function

--- Driver.java
+++ Driver.java
@@ -1,8 +1,9 @@
 public class Driver {
     public static void main(String[] args) {
+        System.out.println(sumOf(10, 5));
     }
 
-    public int sumOf(int x, int y) {
+    public static int sumOf(int x, int y) {
         return x + y;
     }
 }   
